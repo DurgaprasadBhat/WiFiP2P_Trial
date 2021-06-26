811dd7f85dd309e8b66c3bb37a64ee93dd2ecb0c
Durgaprasad <durgaprasad2002bhat@gmail.com>
2021-06-26 13:52:03 +0530

    Fourth Commit (upto 60)

--- app/src/main/java/com/gmail/durgaprasad2002bhat/wifip2pexample/MainActivity.java
+++ app/src/main/java/com/gmail/durgaprasad2002bhat/wifip2pexample/MainActivity.java
@@ -25,7 +25,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -184,5 +187,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         unregisterReceiver(mReciever);
+    }
+
+    public class ServerClass extends Thread{
+        Socket socket;
+        ServerSocket serverSocket;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(8888);
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
